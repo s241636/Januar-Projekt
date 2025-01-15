63195773d9e258a6927c8be61dbf2e389dd41a54
Sifwittus <sif@wittus.eu>
2025-01-15 12:28:00 +0100

    Update train_cnn.py

--- train_cnn.py
+++ train_cnn.py
@@ -15,24 +15,24 @@ def import_data(training_images, testing_images):
     return training_dataloader, testing_dataloader
 
 # Instantierer et tomt cnn
-cnn = cnn.cnn()
+#cnn = cnn.cnn()
 
 # Opstiller et accuracy objekt til at måle hvor god modellen er (beregner andelen af korrekte fordsigelser ud af det totale antal data)
 accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=10) # num_classes er 10, da antallet af klasser(outputtet) i vores cnn er 10(cifrene 0-9)
 
 # Bruger crossentropy som loss-funktion (beregner forskellen mellem modellens output-sandsynligheder og de faktiske tal)
-# Indstiller optimizeren, som opdaterer modellens parametre for at minimuere loss-funktionen
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(cnn.parameters(), lr=0.01, maximize=False)
 
 # Loop'er over 10 epoker, og udregner loss'et og accuracy for hvert.
-def training_loop(training_dataloader, optimizer, loss_fn):
+def training_loop(training_dataloader, loss_fn, model):
     total_loss = 0
     accuracy.reset()
     size = len(training_dataloader)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, maximize=False) # Indstiller optimizeren, som opdaterer modellens parametre for at minimuere loss-funktionen
+
     for images,labels in training_dataloader:
         optimizer.zero_grad()
-        output = cnn(images)
+        output = model(images)
         loss = loss_fn(output, labels)
         total_loss += loss
         loss.backward()
@@ -43,13 +43,13 @@ def training_loop(training_dataloader, optimizer, loss_fn):
     print(f"Avg Training Loss: {avg_loss}")
 
 
-def testing_loop(testing_dataloader, loss_fn):
+def testing_loop(testing_dataloader, loss_fn, model):
     total_loss = 0
     accuracy.reset()
     size = len(testing_dataloader)
     with torch.no_grad():
         for images,labels in testing_dataloader:
-            output = cnn(images)
+            output = model(images)
             loss = loss_fn(output, labels)
             total_loss += loss
             accuracy.update(output,labels)
@@ -58,15 +58,15 @@ def testing_loop(testing_dataloader, loss_fn):
     print(f"Avg Testing Loss: {avg_loss}")
 
 
-def train_test_and_save_model(epoches, filepath, training_images, testing_images):
+def train_test_and_save_model(model, epoches, filepath, training_images, testing_images):
     # Loader den angivne data og laver batches til træning og testing
     training_dataloader, testing_dataloader = import_data(training_images, testing_images)
 
     # Tester og træner over angivet epoker
     for i in range(epoches):
         print(f"Epoch: {i}")
-        testing_loop(testing_dataloader, loss_fn)
-        training_loop(training_dataloader, optimizer, loss_fn)
+        testing_loop(testing_dataloader, loss_fn, model)
+        training_loop(training_dataloader, loss_fn, model)
         print("------------------")
 
     # Gemmer den trænede model i en fil og sletter den, hvis der allerede findes en fil gemt med den trænede model
@@ -77,9 +77,10 @@ def train_test_and_save_model(epoches, filepath, training_images, testing_images
     print(f"Ny cnn gemt som '{filepath}'")
 
 
+model = cnn.cnn()
 epoches = 10
 filepath = "trained_cnn.pth"
 mnist_training_images = MNIST(root='data', transform=ToTensor(), train=True, download=True)
 mnist_testing_images = MNIST(root='data', transform=ToTensor(), train=False, download=True)
 
-train_test_and_save_model(epoches, filepath, mnist_testing_images, mnist_testing_images)
+train_test_and_save_model(model, epoches, filepath, mnist_testing_images, mnist_testing_images)
